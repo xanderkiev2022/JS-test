af644679c6893f0a3a614897eb2b7a0a0d8b1deb
xanderkiev2022 <301019a@gmail.com>
2022-11-09 22:06:18 +0200

    removePotion

--- js/41.js
+++ js/41.js
@@ -27,12 +27,8 @@ const atTheOldToad = {
       if (potion.name === newPotion.name) {
         return `Error! Potion ${newPotion} is already in your inventory!`;
       }
-
       //    this.potions.push(newPotion);
       this.potions.push({ name: newPotion, price: price });
-
-      //const newPotions = { ...potion, newPotion };
-
       return this.potions;
     }
   },
@@ -42,19 +38,21 @@ const atTheOldToad = {
   //----------------------------------//
   removePotion(potionName) {
     for (let potion of this.potions) {
+      //  const potionIndex = this.potions.indexOf(potionName);
+      const potionIndex = this.potions.indexOf(potion);
+      //  if (potionIndex === -1) {
+      if (potion.name === potionName) {
+        //console.log("Зілля є, воно в рядку", potion);
+        this.potions.splice(potionIndex, 1);
+      }
     }
-    const potionIndex = this.potions.indexOf(potionName);
-
-    if (potionIndex === -1) {
-      return `Potion ${potionName} is not in inventory!`;
-    }
-
-    this.potions.splice(potionIndex, 1);
+    return `Potion ${potionName} is not in inventory!`;
   },
 };
 
-console.log(atTheOldToad.getPotions());
 // console.log(atTheOldToad.addPotion("Invisibility", 620));
+console.log(atTheOldToad.removePotion("Dragon breath"));
+console.log(atTheOldToad.getPotions());
 
 //-------
 //   updatePotionName(oldName, newName) {
